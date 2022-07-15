5de90399406d38f22365c33a691832d63e8f00b1
ysaito <shiva.lts239@gmail.com>
2022-07-16 05:05:50 +0900

    feat(lib.rs): add execute booking func
    unit test do not work

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,7 +1,7 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::serde::{Deserialize, Serialize};
-use near_sdk::{env, json_types::U128, near_bindgen, AccountId};
+use near_sdk::{env, json_types::U128, near_bindgen, AccountId, Promise};
 
 #[near_bindgen]
 #[derive(Serialize, Deserialize, Debug, BorshDeserialize, BorshSerialize)]
@@ -58,14 +58,28 @@ impl Rooms {
         );
     }
 
-    // #[result_serializer(borsh)] // TODO: If I don't need it, DELETE
     pub fn get_room(&self, id: String) -> Room {
         self.rooms.get(&id).expect("No Room")
     }
 
-    // #[result_serializer(borsh)] // TODO: If I don't need it, DELETE
     pub fn get_rooms(&self) -> Vec<Room> {
         Vec::from_iter(self.rooms.values())
+    }
+
+    /*
+        return booking Room
+    */
+    #[payable]
+    pub fn booking_room(&mut self, room_id: String) -> Room {
+        let room = self.get_room(room_id.clone());
+        // TODO: 商品がないときのエラー処理
+
+        let deposit = env::attached_deposit();
+        assert!(deposit == room.price.clone().into(), "Not enough tokens");
+        // Delete rooms where bookings are completed.
+        self.rooms.remove(&room_id);
+        Promise::new(room.owner.clone()).transfer(deposit);
+        room
     }
 }
 
@@ -73,80 +87,121 @@ impl Rooms {
 mod tests {
     use super::*;
     use near_sdk::test_utils::{accounts, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
+    use near_sdk::testing_env;
 
     fn near_to_yocto(near_amount: u128) -> U128 {
         U128(near_amount * 10u128.pow(24))
     }
 
     // Allows for modifying the environment of the mocked blockchain
-    fn get_context(predecessor_account_id: AccountId, is_view: bool) -> VMContextBuilder {
+    fn get_context(is_view: bool) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
         builder
+            .account_balance(10)
+            .attached_deposit(10)
             .current_account_id(accounts(0))
-            .signer_account_id(predecessor_account_id.clone())
-            .predecessor_account_id(predecessor_account_id)
+            .predecessor_account_id(accounts(0))
+            .signer_account_id(accounts(1))
             .is_view(is_view);
         builder
     }
 
     #[test]
     fn set_then_get_room() {
-        let mut context = get_context(accounts(1), false);
+        let mut context = get_context(false);
         // Initialize the mocked blockchain
         testing_env!(context.build());
 
         // Set the testing environment for the subsequent calls
-        testing_env!(context.predecessor_account_id(accounts(1)).build());
+        testing_env!(context.predecessor_account_id(accounts(0)).build());
 
         let mut contract = Rooms::default();
         contract.set_room(
             "0".to_string(),
-            "first_room".to_string(),
+            "JAPAN_room".to_string(),
             "test.img".to_string(),
-            "This is First room".to_string(),
+            "This is JAPAN room".to_string(),
             "Japan".to_string(),
             near_to_yocto(10),
         );
 
         let room = contract.get_room("0".to_string());
-        assert_eq!(room.name, "first_room",);
+        assert_eq!(room.name, "JAPAN_room",);
     }
 
     #[test]
     fn set_then_get_rooms() {
-        let mut context = get_context(accounts(1), false);
+        let mut context = get_context(false);
         testing_env!(context.build());
-        testing_env!(context.predecessor_account_id(accounts(1)).build());
+        testing_env!(context.predecessor_account_id(accounts(0)).build());
 
         let mut contract = Rooms::default();
         contract.set_room(
             "0".to_string(),
-            "first_room".to_string(),
+            "JAPAN_room".to_string(),
             "test.img".to_string(),
-            "This is First room".to_string(),
+            "This is JAPAN room".to_string(),
             "Japan".to_string(),
             near_to_yocto(10),
         );
         contract.set_room(
             "1".to_string(),
-            "second_room".to_string(),
+            "USA_room".to_string(),
             "test2.img".to_string(),
-            "This is Second room".to_string(),
+            "This is USA room".to_string(),
             "USA".to_string(),
             near_to_yocto(10),
         );
 
         let rooms = contract.get_rooms();
-        let first_room = rooms.get(0);
+        let japan_room = rooms.get(0);
         // Convert Option<Room> to Room
-        let room_name = first_room.map(|room| &room.name);
-        println!("{:?}", room_name);
-        assert_eq!(room_name.expect("Don't set room"), "first_room",);
+        let room_name = japan_room.map(|room| &room.name);
+        assert_eq!(room_name.expect("Don't set room"), "JAPAN_room",);
 
-        let second_room = rooms.get(1);
-        let room_name = second_room.map(|room| &room.name);
-        println!("{:?}", room_name);
-        assert_eq!(room_name.expect("Don't set room"), "second_room",);
+        let usa_room = rooms.get(1);
+        let room_name = usa_room.map(|room| &room.name);
+        assert_eq!(room_name.expect("Don't set room"), "USA_room",);
     }
+
+    #[test]
+    fn empty_get_rooms() {
+        let mut context = get_context(false);
+        testing_env!(context.build());
+        testing_env!(context.predecessor_account_id(accounts(0)).build());
+        let contract = Rooms::default();
+        let rooms = contract.get_rooms();
+        assert_eq!(rooms.len(), 0);
+    }
+
+    // #[test]
+    // fn booking_room() {
+    //     let mut context = get_context(true);
+    //     testing_env!(context.build());
+    //     testing_env!(context.predecessor_account_id(accounts(0)).build());
+
+    //     let mut contract = Rooms::default();
+    //     contract.set_room(
+    //         "0".to_string(),
+    //         "JAPAN_room".to_string(),
+    //         "test.img".to_string(),
+    //         "This is JAPAN room".to_string(),
+    //         "Japan".to_string(),
+    //         near_to_yocto(1),
+    //     );
+    //     contract.set_room(
+    //         "1".to_string(),
+    //         "USA_room".to_string(),
+    //         "test2.img".to_string(),
+    //         "This is USA room".to_string(),
+    //         "USA".to_string(),
+    //         near_to_yocto(1),
+    //     );
+
+    //     let booking_room = contract.booking_room("0".to_string());
+    //     assert_eq!(booking_room.name, "JAPAN_room");
+
+    //     let rooms = contract.get_rooms();
+    //     assert_eq!(rooms.len(), 1);
+    // }
 }

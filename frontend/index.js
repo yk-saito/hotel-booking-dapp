46c64a7e14f53a4d41f6ea97ee74dd042a3928f0
ysaito <shiva.lts239@gmail.com>
2022-07-17 20:18:29 +0900

    feat(frontend): add method and get_rooms

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { initContract } from './assets/js/near/utils'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { initContract } from "./assets/js/near/utils";
 
-const container = document.querySelector('#root')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+const container = document.querySelector("#root");
+const root = createRoot(container);
 
 window.nearInitPromise = initContract()
   .then(() => {
-    <App />
-    root.render(<App tab="home" />)
+    <App />;
+    root.render(<App tab='home' />);
   })
-  .catch(console.error)
+  .catch(console.error);

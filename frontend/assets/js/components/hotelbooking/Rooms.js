fade422f8559ad96594f5e8d8d848eb6e4dc587a
ysaito <shiva.lts239@gmail.com>
2022-07-24 00:26:00 +0900

    repush: could not save(no space left)

---
+++ frontend/assets/js/components/hotelbooking/Rooms.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState, useCallback } from "react";
+// import { toast } from "react-toastify";
+import AddRoom from "./AddRoom";
+import Room from "./Room";
+// import Loader from "../utils/Loader";
+import { Row } from "react-bootstrap";
+// import { NotificationSuccess, NotificationError } from "../utils/Notifications";
+import { get_all_rooms, createRoom, book_room } from "../../near/utils";
+//...
+
+const Rooms = () => {
+  const [rooms, setRooms] = useState([]);
+
+  //...
+  const getRooms = useCallback(async () => {
+    try {
+      setRooms(await get_all_rooms());
+    } catch (error) {
+      console.log({ error });
+    }
+  });
+  //...
+
+  //...
+  const addRoom = async (data) => {
+    try {
+      // TODO: 余裕あったらNotificationを追加
+      createRoom(data).then((resp) => {
+        console.log("Success!: ", data);
+        getRooms();
+      });
+    } catch (error) {
+      console.log({ error });
+    }
+  };
+  //...
+
+  //...
+  const booking = async (owner_id, room_name, price) => {
+    try {
+      await book_room({
+        owner_id,
+        room_name,
+        price,
+      }).then((resp) => getRooms());
+      console.log("Booking successfully.");
+    } catch (error) {
+      console.log("Failed to booking.");
+    }
+  };
+  //...
+
+  useEffect(() => {
+    getRooms();
+  }, []);
+  return (
+    <>
+      <div className='d-flex justify-content-between align-items-center mb-4'>
+        <h1 className='fs-4 fw-bold mb-0'>Hotel Booking</h1>
+        <AddRoom save={addRoom} />
+      </div>
+      <Row xs={1} sm={2} lg={3} className='g-3  mb-5 g-xl-4 g-xxl-5'>
+        {rooms.map((_room) => (
+          <Room room={{ ..._room }} key={_room.id} booking={booking} />
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Rooms;
---
+++ frontend/assets/js/near/utils.js
@@ -0,0 +1,105 @@
+import { connect, Contract, keyStores, WalletConnection } from "near-api-js";
+import {
+  formatNearAmount,
+  parseNearAmount,
+} from "near-api-js/lib/utils/format";
+import getConfig from "./config";
+
+const GAS = 100000000000000;
+
+const nearConfig = getConfig(process.env.NODE_ENV || "development");
+//...
+
+//...
+// Initialize contract & set global variables
+export async function initContract() {
+  // Initialize connection to the NEAR testnet
+  const near = await connect(
+    Object.assign(
+      { deps: { keyStore: new keyStores.BrowserLocalStorageKeyStore() } },
+      nearConfig
+    )
+  );
+
+  // Initializing Wallet based Account. It can work with NEAR testnet wallet that
+  // is hosted at https://wallet.testnet.near.org
+  window.walletConnection = new WalletConnection(near);
+
+  // Getting the Account ID. If still unauthorized, it's just empty string
+  window.accountId = window.walletConnection.getAccountId();
+
+  // Initializing our contract APIs by contract name and configuration
+  window.contract = await new Contract(
+    window.walletConnection.account(),
+    nearConfig.contractName,
+    {
+      viewMethods: ["get_all_rooms", "get_hotel_rooms", "get_room"],
+      changeMethods: ["set_room", "book_room"],
+    }
+  );
+}
+//...
+//...
+export function logout() {
+  window.walletConnection.signOut();
+  // reload page
+  window.location.replace(window.location.origin + window.location.pathname);
+}
+
+export function login() {
+  window.walletConnection.requestSignIn(nearConfig.contractName);
+}
+
+export async function accountBalance() {
+  return formatNearAmount(
+    (await window.walletConnection.account().getAccountBalance()).total,
+    2
+  );
+}
+
+export async function getAccountId() {
+  return window.walletConnection.getAccountId();
+}
+
+export async function get_all_rooms() {
+  let all_rooms = await window.contract.get_all_rooms();
+  return all_rooms;
+}
+
+export async function get_hotel_rooms(owner_id) {
+  let hotel_rooms = await window.contract.get_hotel_rooms({
+    owner_id: owner_id,
+  });
+  return hotel_rooms;
+}
+
+export async function get_room(owner_id, room_name) {
+  let room = await window.contract.get_room({
+    owner_id: owner_id,
+    room_name: room_name,
+  });
+  return room;
+}
+
+export function createRoom(room) {
+  console.log(room);
+  const timestamp = Date.now().toString();
+  console.log("timestamp: ", timestamp);
+  room.price = parseNearAmount(room.price + "");
+  return window.contract.set_room({
+    timestamp: timestamp,
+    name: room.name,
+    image: room.image,
+    description: room.description,
+    location: room.location,
+    price: room.price,
+  });
+}
+
+export async function book_room({ owner_id, room_name, price }) {
+  await window.contract.book_room(
+    { owner_id: owner_id, room_name: room_name },
+    GAS,
+    price
+  );
+}

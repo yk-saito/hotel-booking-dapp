367a6e02a5faf9bab15bb25d3f01b7b7ed3a1bbb
ysaito <shiva.lts239@gmail.com>
2022-07-27 04:23:10 +0900

    feat(frontend): add NavBar

--- frontend/Routers.js
+++ frontend/Routers.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./assets/js/components/NavBar";
 import Home from "./assets/js/components/Home";
 import RoomManagement from "./assets/js/components/RoomManagement";
 
 const Routers = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/room-management/:address' element={<RoomManagement />} />
